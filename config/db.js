1e22e00fbafa0a3805c8ebfa3e8f25da40cc51de
ffabian <frank.fabian@pucp.edu.pe>
2026-06-24 14:02:48 -0500

    fix: usa variables de entorno para la conexión a BD

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-import _mysql from 'mysql2'; // Usamos la versión con Promesas
+import 'dotenv/config';
+import _mysql from 'mysql2';
 
 const pool = _mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ecommerce_demo',
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'ecommerce_demo',
+  port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
-  connectionLimit: 8,
+  connectionLimit: 2,
   queueLimit: 0
 });
 
--- config/sequelize.js
+++ config/sequelize.js
@@ -1,16 +1,20 @@
+import 'dotenv/config';
 import Sequelize from 'sequelize';
 
-// Configuración de la conexión
-const orm = new Sequelize('ecommerce_demo', 'root', '',
-        {
-            host: 'localhost', 
-            dialect: 'mysql',
-            pool: {
-                max: 2,
-                idle: 10000,
-                acquire: 60000,
-            },
-        }
-    );
+const orm = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'mysql',
+    pool: {
+      max: 2,
+      idle: 10000,
+      acquire: 60000
+    }
+  }
+);
 
 export default orm;
